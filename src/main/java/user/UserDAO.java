23c8ed486ebc41df84e8678a82746ba4d6c1f1ed
jyhwang <stratojy@gmail.com>
2025-02-23 22:33:45 +0900

    feat. 회원가입 servlet 및  DAO
    1. 회원가입에 필요한 UserDAO 와 관련 Servlet 을 만듦.
    2. 회원 가입 페이지에 있는 모든 내용을 필수입력으로 받아서 처리하는 걸로  진행.
    3. web.xml 에서 welcome list를 주석처리했었는데 다시 사용하는 것으로 변경.

---
+++ src/main/java/user/UserDAO.java
@@ -0,0 +1,155 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class UserDAO {
+
+  DataSource dataSource;
+
+  public UserDAO() {
+
+    try {
+      Context initCtx = new InitialContext();
+      Context envCtx = (Context) initCtx.lookup("java:/comp/env");
+      dataSource = (DataSource) envCtx.lookup("jdbc/UserChat");
+
+
+    } catch (Exception e) {
+      e.getMessage();
+    }
+
+  }
+
+  public int login(String userId, String userPassword) {
+
+    Connection conn = null;
+    PreparedStatement pStmt = null;
+    ResultSet rSet = null;
+
+    String sqlQuery =
+        " SELECT user_id AS userId, user_password AS userPassword FROM users WHERE user_id = ? ";
+
+    try {
+      conn = dataSource.getConnection();
+      pStmt = conn.prepareStatement(sqlQuery);
+
+      rSet = pStmt.executeQuery();
+
+      if (rSet.next()) {
+        if (rSet.getString("userPassword").equals(userPassword)) {
+          return 1;// 로그인 성공
+        }
+        return 2;// 비밀번호 x
+      } else {
+        return 0;// 존재하지 않음
+      }
+    } catch (Exception e) {
+      e.getMessage();
+    } finally {
+      try {
+        if (rSet != null) {
+          rSet.close();
+        }
+        if (pStmt != null) {
+          pStmt.close();
+        }
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (Exception e2) {
+        e2.getMessage();
+      }
+    }
+
+    return -1;// 데이터베이스 오류
+  }
+  
+  public int registerCheck(String userId) {
+
+    Connection conn = null;
+    PreparedStatement pStmt = null;
+    ResultSet rSet = null;
+
+    String sqlQuery =
+        " SELECT user_id AS userId FROM users WHERE user_id = ? ";
+
+    try {
+      conn = dataSource.getConnection();
+      pStmt = conn.prepareStatement(sqlQuery);
+
+      rSet = pStmt.executeQuery();
+
+      if (rSet.next() && rSet.getString("userId").equals(userId)) {
+          return 1;// 중복
+        } else {
+        return 0;// OK
+      }
+    } catch (Exception e) {
+      e.getMessage();
+    } finally {
+      try {
+        if (rSet != null) {
+          rSet.close();
+        }
+        if (pStmt != null) {
+          pStmt.close();
+        }
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (Exception e2) {
+        e2.getMessage();
+      }
+    }
+
+    return -1;// 데이터베이스 오류
+  }
+  
+  public int register(UserDTO userDTO) {
+
+    Connection conn = null;
+    PreparedStatement pStmt = null;
+    ResultSet rSet = null;
+
+    String sqlQuery =
+        " INSERT INTO users (user_id, user_password, user_name, user_age, user_gender, user_email)  VALUES (?, ?, ?, ?, ?, ?); ";
+
+    try {
+      conn = dataSource.getConnection();
+      pStmt = conn.prepareStatement(sqlQuery);
+
+      pStmt.setString(1, userDTO.getUserId());
+      pStmt.setString(2, userDTO.getUserPassword());
+      pStmt.setString(3, userDTO.getUserName());
+      pStmt.setInt(4, userDTO.getUserAge());
+      pStmt.setString(5, userDTO.getUserGender());
+      pStmt.setString(6, userDTO.getUserEmail());
+      
+      return pStmt.executeUpdate();
+
+    } catch (Exception e) {
+      e.getMessage();
+    } finally {
+      try {
+        if (rSet != null) {
+          rSet.close();
+        }
+        if (pStmt != null) {
+          pStmt.close();
+        }
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (Exception e2) {
+        e2.getMessage();
+      }
+    }
+
+    return -1;// 데이터베이스 오류
+  }
+}
---
+++ src/main/java/user/UserRegisterServlet.java
@@ -0,0 +1,69 @@
+package user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserRegisterServlet")
+public class UserRegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    request.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/html;charset=UTF-8");
+	    
+	    String userId = request.getParameter("userId");
+	    String userPassword1 = request.getParameter("userPassword1");
+	    String userPassword2 = request.getParameter("userPassword2");
+	    String userName = request.getParameter("userName");
+	    String userAge = request.getParameter("userAge");
+	    String userGender = request.getParameter("userGender");
+	    String userEmail = request.getParameter("userEmail");
+	    
+	    if(userId == null || userId.equals("") || userPassword1 == null || userPassword1.equals("")
+	        || userPassword2 == null || userPassword2.equals("") || userName == null || userName.equals("")
+	        || userAge == null || userAge.equals("") || userGender == null || userGender.equals("")
+	        || userEmail == null || userEmail.equals("")) {
+	      
+	      request.getSession().setAttribute("messageType", "오류메시지");
+	      request.getSession().setAttribute("messageContent", "모든 내용을 입력하세요");
+	      response.sendRedirect("join.jsp");
+	      return;
+	    }
+	    if(!userPassword1.equals(userPassword2)) {
+	      request.getSession().setAttribute("messageType", "오류메시지");
+          request.getSession().setAttribute("messageContent", "비밀번호가 일치하지 않습니다.");
+          response.sendRedirect("join.jsp");
+          return;
+        }
+	    
+	    
+	    UserDTO userDTO = new UserDTO();
+	    
+        userDTO.setUserId(userId);
+        userDTO.setUserPassword(userPassword1);
+        userDTO.setUserName(userId);
+        userDTO.setUserAge(Integer.parseInt(userAge));
+        userDTO.setUserGender(userGender);
+        userDTO.setUserEmail(userEmail);
+        
+        int result = new UserDAO().register(userDTO);
+
+	    if( result == 1) {
+	      request.getSession().setAttribute("messageType", "성공메시지");
+          request.getSession().setAttribute("messageContent", "회원가입에 성공했습니다.");
+          response.sendRedirect("index.jsp");
+          
+	    } else {
+	      request.getSession().setAttribute("messageType", "오류메시지");
+          request.getSession().setAttribute("messageContent", "회원가입에 실패했습니다.");
+          response.sendRedirect("join.jsp");
+          return;
+	    }
+	    
+	}
+
+}

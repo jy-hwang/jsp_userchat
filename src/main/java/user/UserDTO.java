42b3751ca3f0bd8428df3be443d15e5f00a8eb5f
jyhwang <stratojy@gmail.com>
2025-02-23 14:01:05 +0900

    feat. UserDTO 생성
    1. DB에 users 테이블을 만들고 이 데이터를 처리 가공할 수 있는 UserDTO를 생성하였음.

---
+++ src/main/java/user/UserDTO.java
@@ -0,0 +1,98 @@
+package user;
+
+import java.time.LocalDateTime;
+
+public class UserDTO {
+
+  private int userNo;
+
+  private String userId;
+
+  private String userPassword;
+
+  private String userName;
+
+  private int userAge;
+
+  private String userGender;
+
+  private String userEmail;
+
+  private LocalDateTime createdDate;
+
+  private LocalDateTime updatedDate;
+
+  public int getUserNo() {
+    return userNo;
+  }
+
+  public void setUserNo(int userNo) {
+    this.userNo = userNo;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getUserPassword() {
+    return userPassword;
+  }
+
+  public void setUserPassword(String userPassword) {
+    this.userPassword = userPassword;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public int getUserAge() {
+    return userAge;
+  }
+
+  public void setUserAge(int userAge) {
+    this.userAge = userAge;
+  }
+
+  public String getUserGender() {
+    return userGender;
+  }
+
+  public void setUserGender(String userGender) {
+    this.userGender = userGender;
+  }
+
+  public String getUserEmail() {
+    return userEmail;
+  }
+
+  public void setUserEmail(String userEmail) {
+    this.userEmail = userEmail;
+  }
+
+  public LocalDateTime getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(LocalDateTime createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public LocalDateTime getUpdatedDate() {
+    return updatedDate;
+  }
+
+  public void setUpdatedDate(LocalDateTime updatedDate) {
+    this.updatedDate = updatedDate;
+  }
+
+
+}

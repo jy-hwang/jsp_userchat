f38e3ec079ac2ae28572554cede8ba93bfb37bbd
jyhwang <stratojy@gmail.com>
2025-02-27 01:03:57 +0900

    feat. create.ddl
    1. DB 테이블 생성용  ddl

---
+++ sql/create.ddl.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE `users` (
+  `user_no` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_age` tinyint(4) NOT NULL,
+  `user_gender` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_profile` varchar(200) NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_date` timestamp NULL,
+  PRIMARY KEY (`user_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS chats;
+
+CREATE TABLE `chats` (
+  `chat_no` bigint(20) NOT NULL AUTO_INCREMENT,
+  `from_id` varchar(20) NOT NULL,
+  `to_id` varchar(20) NOT NULL,
+  `chat_content`  varchar(200) NOT NULL ,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`chat_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
